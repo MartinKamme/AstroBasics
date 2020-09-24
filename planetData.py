c10e3179bfe7e98c7b3c3115d22b0aa49476d9d4
Martin Kamme <martinkamme@gmail.com>
2020-09-24 14:17:56 -0700

    Add files via upload

---
+++ planetData.py
@@ -0,0 +1,21 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 02:36:47 2020
+
+@author: marti
+"""
+
+G_meters = 6.67408*10**-11
+G_km = G_meters*10**-9
+
+sun = {
+       'name':'Sun',
+       'mu':1.32712*10**11,
+       'radius':695700.0,
+       'mass':1.989*10**30}
+
+earth = {
+        'name': 'Earth',
+        'mu': 398600,
+        'mass': 5.972*10**24,
+        'radius': 6378.0}
